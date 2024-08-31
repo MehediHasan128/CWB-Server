9a2d24000d99c85ac1f2087809ff3a71a0fb17bc
MehediHasan128 <mehedihasan12926@gmail.com>
2024-08-31 18:42:54 +0600

    add cercel.json file

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,11 @@ import NotFound from './app/middlwares/NotFound';
 const app: Application = express();
 
 app.use(express.json());
-app.use(cors());
+
+app.use(cors({
+  origin: 'http://localhost:5173', 
+  credentials: true
+}));
 
 app.use('/api/v1', router)
 
